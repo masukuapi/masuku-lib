5d31226bc989ba7655b9cf236e0d8f3fdc10618b
Adrin Jose C T <adrinjosect@gmail.com>
2024-04-13 16:03:40 +0530

    Started on the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    './templates/**/*.html',
-  ],
+  content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        screenWithNav: "calc(100vh - 6rem)",
+      },
+      colors: {
+        background: "#051931",
+        backgroundSecondary: "#111B23",
+        textInactive: "#757575",
+        primary: "#97C7FF",
+        tertiary: "#000715",
+      },
+    },
   },
   plugins: [],
-}
+};
