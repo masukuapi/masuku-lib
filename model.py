1682375b9bee021639d8ef4a29349943b4df8d6d
Glitchyi <advaithnarayanan8@gmail.com>
2024-04-13 23:25:30 +0530

    fixed onnx runtime and setup proper :)

--- model.py
+++ model.py
@@ -1,35 +1,29 @@
 import cv2
-import torch
 import pathlib
 import numpy as np
 import onnxruntime as ort
 
 #Passing the input to the model
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 # Load the ONNX model
-ort_session = ort.InferenceSession('models\\best.onnx')
+ort_session = ort.InferenceSession('models/best.onnx')
 
-input_img = cv2.imread('man.jpg')
+input_img = cv2.imread('test.jpg')
 input_img = cv2.resize(input_img, (640, 640))
-input_img = torch.from_numpy(input_img).permute(2, 0, 1).unsqueeze(0).float()
-input_img = input_img.to(device)
+input_img = np.expand_dims(np.transpose(input_img, (2, 0, 1)), axis=0).astype(float)
 
 # Convert to float32
-input_img = input_img.float()
-
-# Run the model with the input image
-results = ort_session.run(None, {ort_session.get_inputs()[0].name: input_img.numpy()})
+input_img = input_img.astype('float32')
+results = ort_session.run(None, {ort_session.get_inputs()[0].name: input_img})
 results = results[0][0]
-#print(results)
-covered_prob = results[:, 0].max() # Maximum probability of 'covered' class
-not_covered_prob = results[:, 1].max() # Maximum probability of 'not-covered' class
-#print(covered_prob, not_covered_prob)
+covered_prob = results[:, 0].max()
+not_covered_prob = results[:, 1].max()
 if covered_prob > not_covered_prob:
     result = "not-covered"
+    print(f"{result} | NC: {not_covered_prob} | C: {covered_prob} ")
 else:
     result = "covered"
-print(f"The face in the image is {result}.")
+    print(f"{result} | NC: {not_covered_prob} | C: {covered_prob} ")
