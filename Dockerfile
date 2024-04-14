e9c1f73b56063370bb3287e53099e6dded43055a
Glitchyi <advaithnarayanan8@gmail.com>
2024-04-14 20:11:18 +0530

    Update .dockerignore and Dockerfile, add Docker build workflow, and update build.sh script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Python image as the base image
-FROM python:3.11.3-slim
+FROM python:3.11.2-slim
 
 # Set the working directory in the container
 WORKDIR /app
--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 ver='0.0.1'
-sudo docker build -t masuku:$ver .
-sudo docker tag masuku:$ver masuku:latest
+image_name='masuku'
+docker_username='glitchyi'
+
+sudo docker build -t $docker_username/$image_name:$ver .
+sudo docker tag $docker_username/$image_name:$ver $docker_username/$image_name:latest
+sudo docker push $docker_username/$image_name:$ver
